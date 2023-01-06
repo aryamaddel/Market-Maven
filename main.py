354e1ec7dc06f98f48b9db58435cd9e507dd760c
Arya Maddel <84805906+aryamaddel@users.noreply.github.com>
2023-01-07 01:48:18 +0530

    Update main.py

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ driver.get('https://www.amazon.in')
 
 searchBar = driver.find_element(By.ID, 'twotabsearchtextbox')
 driver.implicitly_wait(5)
-searchBar.send_keys('iphone')
+searchBar.send_keys('ps5')
 searchBar.submit()
 
 
